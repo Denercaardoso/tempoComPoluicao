74ffa4e052ff48b2183d3b360a6922cf59b57a2f
unknown <denercardoso.2013@gmail.com>
2022-08-21 21:04:13 -0300

    tempo e poluicao

---
+++ src/app/poluicao/poluicao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poluicao',
+  templateUrl: './poluicao.component.html',
+  styleUrls: ['./poluicao.component.scss']
+})
+export class PoluicaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
